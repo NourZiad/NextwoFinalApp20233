4c31a5642bbd422b61b7908372556d8469d4fb2c
NOUR <noor.alhaj669@gmail.com>
2023-04-02 00:47:21 +0300

    Changeee

---
+++ NextwoFinalApp2023/ViewComponents/PartnerViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NextwoFinalApp2023.Data;
+
+namespace NextwoFinalApp2023.ViewComponents
+{
+    public class PartnerViewComponent : ViewComponent
+    {
+        private FinalDbContext db;
+        public PartnerViewComponent(FinalDbContext _db)
+        {
+            db = _db;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var data = db.Partners.OrderByDescending(x => x.UserId).Take(4);
+            return View(data);
+        }
+    }
+}
